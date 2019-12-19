c3af52ef3543019f44739ba87085a91a4b037be3
tonyfang <q710370@gmail.com>
2019-12-19 15:20:08 +0800

    Add function record cost time

--- app.js
+++ app.js
@@ -13,31 +13,41 @@ app.set('view engine', 'handlebars')
 
 // 列出全部 Todo
 app.get('/', (req, res) => {
-  res.render('index')
+  console.time('time')
   console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  res.render('index')
+  console.timeEnd('time')
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
-  res.send('新增 Todo 頁面')
+  console.time('time')
   console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  res.send('新增 Todo 頁面')
+  console.timeEnd('time')
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
-  res.send('顯示一筆 Todo')
+  console.time('time')
   console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  res.send('顯示一筆 Todo')
+  console.timeEnd('time')
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
-  res.send('新增一筆  Todo')
+  console.time('time')
   console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  res.send('新增一筆  Todo')
+  console.timeEnd('time')
 })
 
 app.delete('/:id/delete', (req, res) => {
-  res.send('刪除 Todo')
+  console.time('time')
   console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  res.send('刪除 Todo')
+  console.timeEnd('time')
 })
 
 app.listen(port, () => {
