6946ee7538945cf4c5158e156b151e5da949ac1e
tonyfang <q710370@gmail.com>
2020-01-16 19:38:55 +0800

    Add new function

--- app.js
+++ app.js
@@ -10,42 +10,41 @@ app.use(methodOverride('_method'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+function recordtime(req) {
+  console.time('time')
+  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+}
 
 // 列出全部 Todo
 app.get('/', (req, res) => {
-  console.time('time')
-  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  recordtime(req)
   res.render('index')
   console.timeEnd('time')
 })
 
 // 新增一筆 Todo 頁面
 app.get('/new', (req, res) => {
-  console.time('time')
-  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  recordtime(req)
   res.send('新增 Todo 頁面')
   console.timeEnd('time')
 })
 
 // 顯示一筆 Todo 的詳細內容
 app.get('/:id', (req, res) => {
-  console.time('time')
-  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  recordtime(req)
   res.send('顯示一筆 Todo')
   console.timeEnd('time')
 })
 
 // 新增一筆  Todo
 app.post('/', (req, res) => {
-  console.time('time')
-  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  recordtime(req)
   res.send('新增一筆  Todo')
   console.timeEnd('time')
 })
 
 app.delete('/:id/delete', (req, res) => {
-  console.time('time')
-  console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }) + ' | ' + req.method + ' from ' + req.originalUrl)
+  recordtime(req)
   res.send('刪除 Todo')
   console.timeEnd('time')
 })
